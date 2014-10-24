451693d2958ab65b1dfc4ecd304d5ffde6d8bb77
Jake Drahos <jake@laptop.site>
2014-10-24 01:28:54 -0500

    Fixed ad serving issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 require 'net/http'
 class ApplicationController < ActionController::Base
-  #@adserver_uri = "http://ads.teamN.isucdc.com/get_ad_url.php"
-  @adserver_uri = nil
-  
-  
-  @url_of_ad_image = nil
   before_filter do 
+    adserver_uri = nil
+    @url_of_ad_image = nil
+    
+    # Edit the following line and uncomment it 
+    # adserver_uri = "http://ads.teamN.isucdc.com/index.php"
+    
     unless @adserver_uri.nil?
-      @url_of_ad_image = Net::HTTP.get URI(@adserver_uri)
+      @url_of_ad_image = Net::HTTP.get URI(adserver_uri)
     end
   end
 end
