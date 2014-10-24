1e5691c837ebb37bdcd45b9a5ce902e5c3777cfe
root <root@isucdc.com>
2014-10-24 10:34:04 -0500

    Fixed some things
    I can't remember exactly what

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     @url_of_ad_image = nil
     
     # Edit the following line and uncomment it 
-    # adserver_uri = "http://your_ad_server_here/index.php"
+    # adserver_uri = "http://<your_ad_server_goes_here>/index.php"
     
     unless adserver_uri.nil?
       @url_of_ad_image = Net::HTTP.get URI(adserver_uri)
