ac073da869bb6b73ce667715a3c3c9d7319f47d9
Jake Drahos <jake@laptop.site>
2014-10-24 01:51:02 -0500

    Fixed typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     # Edit the following line and uncomment it 
     # adserver_uri = "http://ads.teamN.isucdc.com/index.php"
     
-    unless @adserver_uri.nil?
+    unless adserver_uri.nil?
       @url_of_ad_image = Net::HTTP.get URI(adserver_uri)
     end
   end
